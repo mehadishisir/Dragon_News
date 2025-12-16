1de9d83165297f8d0f2ed4c9ab221f9fe97405b1
mehadi <mehadishisir@gmail.com>
2025-12-16 23:20:46 +0600

    add navigate

--- src/pages/HomeNews.jsx
+++ src/pages/HomeNews.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Navigate } from "react-router";
 
 const HomeNews = () => {
-  return <div>home news</div>;
+  return (
+    <div>
+      <Navigate to="/category/1"></Navigate>
+    </div>
+  );
 };
 
 export default HomeNews;
