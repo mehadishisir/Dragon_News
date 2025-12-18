0b30dc3ec96a0f077ad3cbd38f44a63c21db003f
mehadi <mehadishisir@gmail.com>
2025-12-18 17:02:49 +0600

    add firebase

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDo91BQc8jyQ4ONw8ZWSJcjOYiiydR-4BM",
+  authDomain: "dragon-news-bangladesh-bef84.firebaseapp.com",
+  projectId: "dragon-news-bangladesh-bef84",
+  storageBucket: "dragon-news-bangladesh-bef84.firebasestorage.app",
+  messagingSenderId: "5127978836",
+  appId: "1:5127978836:web:7ae44972dda5c262b952c1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
